7e45f93bab6cbe33087e152f041431c4fd83966f
will short <willofmetal85@gmail.com>
2021-10-01 16:43:50 -0500

    added heroku stuff

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(server.listening + " on port " + port));
